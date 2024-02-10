f83712bfd3ac22b628790983cabd714fb9d1a768
Alvin Pratama <palveeen22@gmail.com>
2024-02-10 10:17:46 +0700

    fix: camera filter

--- src/components/CameraFilter.tsx
+++ src/components/CameraFilter.tsx
@@ -1,9 +1,9 @@
-import { useEffect, useRef } from "react";
-import { toPng } from "html-to-image";
-import p5 from "p5";
+// import { useEffect, useRef } from "react";
+// import { toPng } from "html-to-image";
+// import p5 from "p5";
 
-const CameraFilter = () => {
-	return <div>CameraFilter</div>;
-};
+// const CameraFilter = () => {
+// 	return <div>CameraFilter</div>;
+// };
 
-export default CameraFilter;
+// export default CameraFilter;
